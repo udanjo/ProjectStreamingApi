32db13d5d129e3c4eb64aec1b11ee4364ddc015d
ueverson diego soares pedroso <udanjo_hyuuga@hotmail.com>
2022-04-20 11:04:00 -0300

    chore: Initial Commit

---
+++ ProjectStreamingApi/Controllers/TodoController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectStreamingApi.Application.Models;
+using ProjectStreamingApi.Results;
+using System.Collections.Concurrent;
+using System.Text.Json;
+
+namespace ProjectStreamingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private static ConcurrentBag<StreamWriter> _clients = new ConcurrentBag<StreamWriter>();
+        private static List<ItemModel> _itens = new List<ItemModel>();
+
+        [HttpGet]
+        public ActionResult<List<ItemModel>> Get() => _itens;
+
+        [HttpPost]
+        public async Task<ActionResult<ItemModel>> Post([FromBody] ItemModel request)
+        {
+            if (request == null)
+                return BadRequest();
+
+            if (request.Id == Guid.Empty) request.Id = Guid.NewGuid();
+
+            _itens.Add(request);
+
+            await WriteOnStream(request, "Added");
+
+            return request;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var client = _itens.SingleOrDefault(i => i.Id == id);
+            if (client != null)
+            {
+                _itens.Remove(client);
+
+                await WriteOnStream(client, "removed");
+
+                return Ok(new { Description = "Item removed" });
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("streaming")]
+        public IActionResult Streaming()
+        {
+            return new StreamResult(
+                (stream, cancelToken) =>
+                {
+                    var wait = cancelToken.WaitHandle;
+                    var client = new StreamWriter(stream);
+                    _clients.Add(client);
+
+                    wait.WaitOne();
+
+                    StreamWriter ignore;
+                    _clients.TryTake(out ignore);
+                },
+                HttpContext.RequestAborted);
+        }
+
+        private async Task WriteOnStream(ItemModel data, string action)
+        {
+            foreach (var client in _clients)
+            {
+                string jsonData = string.Format("{0}\n", JsonSerializer.Serialize(new { data, action }));
+                await client.WriteAsync(jsonData);
+                await client.FlushAsync();
+            }
+        }
+    }
+}
---
+++ ProjectStreamingApi/Models/ItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectStreamingApi.Application.Models
+{
+    public class ItemModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {IsComplete}";
+        }
+    }
+}
